c8e2ea0fcb364a27154873ced4354136766751aa
aaron <aaron.gerteis@gmail.com>
2025-12-02 12:09:06 +0100

    Add project files.

---
+++ AdressableOverlayVR/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Valve.VR;
+
+class Program
+{
+    static void Main()
+    {
+        EVRInitError err = EVRInitError.None;
+        OpenVR.Init(ref err, EVRApplicationType.VRApplication_Overlay);
+
+        Console.WriteLine("OpenVR init: " + err);
+    }
+}
+
+namespace AdressableOverlay
+{
+    public partial class WristOverlay()
+    {
+         InitializeComponent
+    }
+}
