d449a4754ca93a741ff3af54c651ee9c55d68d68
aaron <aaron.gerteis@gmail.com>
2025-12-03 15:22:52 +0100

    commit everything

--- AdressableOverlayVR/Forms/AOVR.Designer.cs
+++ AdressableOverlayVR/Forms/AOVR.Designer.cs
@@ -1,14 +1,35 @@
-﻿namespace AdressableOverlayVR.Forms
+﻿using System.Drawing.Drawing2D;
+
+namespace AdressableOverlayVR.Forms
 {
     partial class AOVR
     {
         private void InitializeComponent()
         {
             this.SuspendLayout();
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1080, 720);
             this.Name = "AOVR";
             this.ResumeLayout(false);
+        }
+        public void CustomizeLinearGradients(PaintEventArgs e)
+        {
+            LinearGradientBrush linGrBrush = new LinearGradientBrush(
+               new Point(0, 10),
+               new Point(200, 10),
+               Color.FromArgb(255, 0, 0, 0),     // Opaque black
+               Color.FromArgb(255, 255, 0, 0));  // Opaque red
+
+            float[] relativeIntensities = { 0.0f, 0.5f, 1.0f };
+            float[] relativePositions = { 0.0f, 0.2f, 1.0f };
+
+            //Create a Blend object and assign it to linGrBrush.
+            Blend blend = new Blend();
+            blend.Factors = relativeIntensities;
+            blend.Positions = relativePositions;
+            linGrBrush.Blend = blend;
+
+            e.Graphics.FillEllipse(linGrBrush, 0, 30, 200, 100);
+            e.Graphics.FillRectangle(linGrBrush, 0, 155, 500, 30);
         }
     }
 }
